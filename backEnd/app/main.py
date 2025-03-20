6615bf537b75426e49e5d9520e87f0b692bc2a6e
kevin-467 <3077384244@qq.com>
2025-03-20 23:12:55 +0800

    调整main.py

--- backEnd/app/main.py
+++ backEnd/app/main.py
@@ -5,12 +5,12 @@ project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, project_root)
 
 from fastapi import FastAPI
-from backEnd.app.database import test_connection
-from backEnd.app.routers.db_router import db_router
+# from backEnd.app.database import test_connection
+# from backEnd.app.routers.db_router import db_router
 from backEnd.app.routers.login_router import login_router
 
 app = FastAPI()
-app.include_router(db_router)
+# app.include_router(db_router)
 app.include_router(login_router)
 
 
@@ -20,10 +20,11 @@ def welcome():
 
 
 # 启动时测试数据库连接
-try:
-    test_connection()
-except Exception as e:
-    print(f"数据库连接失败:{e}")
+# try:
+#     test_connection()
+# except Exception as e:
+#     print(f"数据库连接失败:{e}")
 
-for route in app.routes:
-    print(route.path)
+# for route in app.routes:
+#     print(route.path)
+
