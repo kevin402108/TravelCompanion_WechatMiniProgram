ece4a3b9c46f178a56645d5c97c9f583ac3838d6
kevin402108 <3077384244@qq.com>
2025-07-17 17:10:16 +0800

    fix some known bugs

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,3 +1,5 @@
+/// <refrence types="miniprogram-api-typings" />
+/// <refrence path="./typings/wx.d.ts" />
 import loginUtils from './utils/loginUtils'
 
 App({
@@ -5,10 +7,8 @@ App({
     defaultAvatarUrl:
       "https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0",
   },
-
   onLaunch: function () {
     //检查用户登录情况
     loginUtils.checkLogin(1)
-    
-  },
+    },
 });
