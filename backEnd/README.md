7b2da9fe220b2472b833bc8a7a68fcf33e37729c
kevin402108 <3077384244@qq.com>
2025-08-06 01:26:14 +0800

    完善后端工程化管理(poetry)

---
+++ backEnd/README.md
@@ -0,0 +1,9 @@
+# 旅伴奇遇工坊
+
+- 项目端部分采用poetry(version:2.1.4)进行依赖管理和打包，主要包含以下文件/文件夹：
+  - .venv: 虚拟环境文件夹
+  - app：后端部分的主要代码
+  - config：后端部分的配置文件
+  - README.md：项目说明文件
+  - poetry.lock&pyproject.toml：依赖管理文件
+  - requirements.txt: 项目依赖的三方库清单
--- backEnd/config/db_config.py
+++ backEnd/config/db_config.py
@@ -1,12 +1,7 @@
 import os
-from dotenv import load_dotenv
 
 # 当前项目所处环境
 ENVIRONMENT = os.getenv("ENVIRONMENT", "production")
-if ENVIRONMENT == "development":
-    # print("当前处于开发环境")
-    env_path = os.path.join(os.path.dirname(__file__), ".env")
-    load_dotenv(env_path)
 
 DB_USER = os.getenv("DB_USER")
 DB_PWD = os.getenv("DB_PWD")
@@ -14,14 +9,10 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-
-# print(DB_USER, DB_PWD, DB_HOST, DB_PORT, DB_NAME)
 if not all([DB_USER, DB_PWD, DB_HOST, DB_PORT, DB_NAME]):
     raise ValueError("缺失部分或全部数据库环境变量，请检查后重试！")
 
-# 数据库url
 DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-# print(DB_URL)
 
 # 生产环境连接配置
 if ENVIRONMENT == "production":
